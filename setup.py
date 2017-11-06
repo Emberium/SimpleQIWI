424a5cdc6f8b3f7e78da6daf3020ee35a0ad49c0
Emberium <31995917+Emberium@users.noreply.github.com>
2017-11-06 22:05:10 +0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from setuptools import setup
 
 setup(
     name='SimpleQIWI',
-    version='0.5',
+    version='0.7',
     author='Emberium',
     description='QIWI API for Humans',
+    long_description='See https://github.com/Emberium/SimpleQIWI',
     url='https://github.com/Emberium/SimpleQIWI',
     install_requires=['requests', 'uuid'],
     packages=['SimpleQIWI']
